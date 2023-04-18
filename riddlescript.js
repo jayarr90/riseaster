9155a110aae9516e78e6cd1f70c486cf453c23a6
jayarr90 <129124025+jayarr90@users.noreply.github.com>
2023-04-18 06:12:17 +0100

    Add files via upload

--- riddlescript.js
+++ riddlescript.js
@@ -3,7 +3,7 @@ const answerInput = document.getElementById("answer-input");
 
 submitButton.addEventListener("click", function() {
 	if (answerInput.value.toLowerCase() === "lily") {
-		window.location.href = "scramble.html";
+		window.location.href = "story.html";
 	} else {
 		alert("Sorry, that's incorrect. Please try again.");
 	}
