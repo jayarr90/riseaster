a6e89f9119e9c990e2f91af7aef65e3905983850
jayarr90 <129124025+jayarr90@users.noreply.github.com>
2023-04-18 06:24:08 +0100

    Add files via upload

--- scramble.js
+++ scramble.js
@@ -1,4 +1,4 @@
-const words = ["easter", "bunny", "chocolate", "basket", "cadburys", "creme-egg", "hunt", "chick", "nestle"];
+const words = ["easter", "bunny", "chocolate", "basket", "cadburys", "eggs", "hunt", "chick", "lily"];
 let shuffledWord = "";
 let wordIndex = 0;
 let correctAnswers = 0;
