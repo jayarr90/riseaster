017ec8243e1582f7b1103332a893eecb9db5a70c
jayarr90 <129124025+jayarr90@users.noreply.github.com>
2023-11-21 11:45:53 +0000

    Update riddlescript.js

--- riddlescript.js
+++ riddlescript.js
@@ -2,7 +2,7 @@ const submitButton = document.getElementById("submit-button");
 const answerInput = document.getElementById("answer-input");
 
 submitButton.addEventListener("click", function() {
-	if (answerInput.value.toLowerCase() === "lily") {
+	if (answerInput.value.toLowerCase() === "tinsel") {
 		window.location.href = "story.html";
 	} else {
 		alert("Sorry, that's incorrect. Please try again.");
