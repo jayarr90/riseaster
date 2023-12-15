96396ae5db289ea8d63b04828e295f1aacbe4e6c
jayarr90 <129124025+jayarr90@users.noreply.github.com>
2023-12-15 10:47:25 +0000

    Update scramble.js

--- scramble.js
+++ scramble.js
@@ -1,4 +1,31 @@
-const words = ["snow", "bauble", "chocolate", "tinsel", "rudolph", "sleigh-bells", "santa claus", "winter wonderland", "presents"];
+document.addEventListener('DOMContentLoaded', function () {
+    // Set the date we're counting down to (current time + 5 minutes)
+    var countDownDate = new Date().getTime() + 5 * 60 * 1000;
+
+    // Update the countdown every 1 second
+    var x = setInterval(function () {
+        // Get the current date and time
+        var now = new Date().getTime();
+
+        // Calculate the remaining time
+        var distance = countDownDate - now;
+
+        // Calculate minutes and seconds
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the countdown
+        document.getElementById('countdown').innerHTML = minutes + 'm ' + seconds + 's ';
+
+        // If the countdown is over, display a message
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById('countdown').innerHTML = 'EXPIRED';
+        }
+    }, 1000);
+});
+
+const words = ["snow", "bauble", "chocolate", "tinsel", "rudolph", "sleigh", "grinch", "wonderland", "presents"];
 let shuffledWord = "";
 let wordIndex = 0;
 let correctAnswers = 0;
